bb5e26d2fce654754cf5660d74e521e65bd37e5a
thiennlinh <nguyenthienlinhptit@gmail.com>
2020-12-25 13:45:31 +0700

    Fix error registation

--- root/settings/registration.py
+++
@@ -1,8 +0,0 @@
-import os
-
-FRONTEND_ROOT_URL = os.environ.get("APPS_ENVIRONMENT_ROOT_URL")
-
-REST_REGISTRATION = {
-    'RESET_PASSWORD_VERIFICATION_URL': '{}/forgot'.format(FRONTEND_ROOT_URL),
-    'USER_VERIFICATION_FLAG_FIELD': 'is_active',
-}
