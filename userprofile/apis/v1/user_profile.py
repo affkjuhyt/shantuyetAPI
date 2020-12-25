3fd20bb761b290664bdfe8be83a11023fad965c6
thiennlinh <nguyenthienlinhptit@gmail.com>
2020-12-25 14:23:36 +0700

    Action show teas of owner

--- userprofile/apis/v1/user_profile.py
+++ userprofile/apis/v1/user_profile.py
@@ -1,10 +1,14 @@
 import logging
 
+from rest_framework.decorators import action
 from rest_framework.parsers import JSONParser, MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet, ReadOnlyModelViewSet
 
+from request.models import Request
 from root.authentications import BaseUserJWTAuthentication
+from teas.models import Teas
+from teas.serializers import TeasSerializer
 from userprofile.models import UserProfile
 from userprofile.serializers import UserProfileSerializer
 
@@ -18,7 +22,27 @@ class UserPublicView(GenericViewSet):
     parser_classes = [JSONParser, MultiPartParser, FormParser]
 
     def get_queryset(self):
-        return UserProfile.objects.filter().all()
+        return UserProfile.objects.filter(user_id=self.request.user.id).all()
+
+    # @action(detail=True, methods=['get'], url_path='owner_teas', serializer_class=TeasSerializer)
+    # def get_owner_tea(self, *args, **kwargs):
+    #     owner = self.get_object()
+    #     teas = Teas.objects.filter(owner=owner)
+    #     serializer = TeasSerializer(teas, many=True)
+    #     return Response(serializer.data)
+    #
+    # @action(detail=True, methods=['get'], url_path='secondary_owner_teas',
+    #         serializer_class=TeasSerializer)
+    # def get_secondary_owner_teas(self, *args, **kwargs):
+    #     secondary_owner = self.get_object()
+    #     requests = Request.objects.filter(secondary_owner=secondary_owner)
+    #     if len(requests) == 0:
+    #         return Response({"message": "Khong co san pham"})
+    #
+    #     for request in requests:
+    #         teas = Teas.objects.filter(id=request.id)
+    #         serializer = TeasSerializer(teas, many=True)
+    #         return Response(serializer.data)
 
     def list(self, request, *args, **kwargs):
         user_profile = self.get_queryset().first()
