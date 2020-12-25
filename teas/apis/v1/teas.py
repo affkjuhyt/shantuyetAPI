8097419a83710885b7d97da16cdd58073cce0b66
thiennlinh <nguyenthienlinhptit@gmail.com>
2020-12-25 17:06:18 +0700

    Api show info teas

--- teas/apis/v1/teas.py
+++ teas/apis/v1/teas.py
@@ -1,13 +1,18 @@
 import logging
 
 from rest_framework import generics
+from rest_framework.decorators import action
 from rest_framework.parsers import JSONParser, MultiPartParser, FormParser
 from rest_framework.permissions import AllowAny
-from userprofile.permissions import OwnerOnly, SecondaryOwnerOnly
+from rest_framework.response import Response
+
+from request.models import Request
+from userprofile.models import Owner, SecondaryOwner
 from rest_framework.viewsets import ReadOnlyModelViewSet, ViewSetMixin
 
 from teas.models import Teas
 from teas.serializers import TeasSerializer
+from userprofile.serializers import OwnerSerializer, SecondaryOwnerSerializer
 
 logger = logging.getLogger(__name__.split('.')[0])
 
@@ -28,4 +33,23 @@ class TeasAdminView(ViewSetMixin, generics.RetrieveUpdateAPIView, generics.ListC
     parser_classes = [JSONParser, MultiPartParser, FormParser]
 
     def get_queryset(self):
-        return Teas.objects.filter()
+        return Teas.objects.filter().all()
+
+    @action(detail=True, methods=['get'], url_path='info_owner', serializer_class=OwnerSerializer)
+    def get_info_owner(self, *args, **kwargs):
+        tea = self.get_object()
+        owner = Owner.objects.filter(teas=tea)
+        serializer = OwnerSerializer(owner, many=True)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['get'], url_path='info_secondary_owner', serializer_class=OwnerSerializer)
+    def get_info_secondary_owner(self, *args, **kwargs):
+        tea = self.get_object()
+        requests = Request.objects.filter(tea=tea)
+        if len(requests) == 0:
+            return Response({"message": "Khong co chu so huu thu cap"})
+
+        for request in requests:
+            secondary_owner = SecondaryOwner.objects.filter(secondary_owner=request)
+            serializer = SecondaryOwnerSerializer(secondary_owner, many=True)
+            return Response(serializer.data)
