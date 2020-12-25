55e2e9c2d12ea6adc0b028f8c40c0dc2c5b44b51
thiennlinh <nguyenthienlinhptit@gmail.com>
2020-12-25 10:11:46 +0700

    Add view admin

--- userprofile/admin.py
+++ userprofile/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from userprofile.models import UserProfile
+from userprofile.models import UserProfile, Owner, SecondaryOwner
 
 
 class UserProfileAdmin(admin.ModelAdmin):
@@ -12,4 +12,27 @@ class UserProfileAdmin(admin.ModelAdmin):
 
 
 admin.site.register(UserProfile , UserProfileAdmin)
+
+
+class OwnerAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'avatar', 'fullname', 'user_type', 'position', 'city', 'phone_number',
+                    'current_status',
+                    'date_modified', 'date_added']
+    search_fields = ['fullname', 'phone_number']
+    raw_id_fields = ['user']
+    list_filter = ['current_status']
+
+
+admin.site.register(Owner, OwnerAdmin)
+
+
+class SecondaryOwnerAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'fullname', 'phone_number', 'user_type', 'mission', 'current_status', 'date_modified',
+                    'date_added']
+    search_fields = ['fullname', 'phone_number']
+    raw_id_fields = ['user']
+    list_filter = ['current_status']
+
+
+admin.site.register(SecondaryOwner, SecondaryOwnerAdmin)
 
