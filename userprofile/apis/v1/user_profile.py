cb5bc7e5c4d6f10bb464a8fef0e5d8f2ee73657a
thiennlinh <nguyenthienlinhptit@gmail.com>
2020-12-25 14:08:14 +0700

    Fix value return method

--- userprofile/apis/v1/user_profile.py
+++ userprofile/apis/v1/user_profile.py
@@ -1,6 +1,7 @@
 import logging
 
 from rest_framework.parsers import JSONParser, MultiPartParser, FormParser
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet, ReadOnlyModelViewSet
 
 from root.authentications import BaseUserJWTAuthentication
@@ -10,11 +11,16 @@ from userprofile.serializers import UserProfileSerializer
 logger = logging.getLogger(__name__.split('.')[0])
 
 
-class UserPublicView(ReadOnlyModelViewSet):
+class UserPublicView(GenericViewSet):
     serializer_class = UserProfileSerializer
     authentication_classes = [BaseUserJWTAuthentication]
     filter_fields = []
     parser_classes = [JSONParser, MultiPartParser, FormParser]
 
     def get_queryset(self):
-        return UserProfile.objects.filter(user_id=self.request.user.id).all()
+        return UserProfile.objects.filter().all()
+
+    def list(self, request, *args, **kwargs):
+        user_profile = self.get_queryset().first()
+        serializer = self.get_serializer(user_profile)
+        return Response(serializer.data)
