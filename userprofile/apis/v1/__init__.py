05106e2dba46a75ff6dfdc7b9e677a6bbc1e9c76
thiennlinh <nguyenthienlinhptit@gmail.com>
2021-01-06 14:18:07 +0700

    Update info-user

--- userprofile/apis/v1/__init__.py
+++ userprofile/apis/v1/__init__.py
@@ -1,3 +1,3 @@
 from .owner import OwnerPublicView, OwnerAdminView
 from .secondary_owner import SecondaryOwnerPublicView, SecondaryOwnerAdminView
-from .user_profile import UserPublicView
+from .user_profile import UserPublicView, UpdateInfo
--- userprofile/apis/v1/user_profile.py
+++ userprofile/apis/v1/user_profile.py
@@ -1,8 +1,10 @@
 import logging
 
+from rest_framework import status, generics
 from rest_framework.parsers import JSONParser, MultiPartParser, FormParser
 from rest_framework.response import Response
-from rest_framework.viewsets import GenericViewSet, ReadOnlyModelViewSet
+from rest_framework.viewsets import GenericViewSet, ReadOnlyModelViewSet, ViewSetMixin
+from django.contrib.auth.models import User
 
 from root.authentications import BaseUserJWTAuthentication
 from userprofile.models import UserProfile
@@ -24,3 +26,24 @@ class UserPublicView(ReadOnlyModelViewSet):
         user_profile = self.get_queryset().first()
         serializer = self.get_serializer(user_profile)
         return Response(serializer.data)
+
+
+class UpdateInfo(ReadOnlyModelViewSet):
+    authentication_classes = [BaseUserJWTAuthentication]
+
+    def post(self, request, *args, **kwargs):
+        if not request.data:
+            return Response({'Error': "Please provide username/password"}, status=status.HTTP_400_BAD_REQUEST)
+
+        username = request.data['username']
+        password = request.data['password']
+        user = User.objects.filter(id=request.user.id).first()
+
+        if User.objects.filter(username=username).exists():
+            return Response({'Error': 'Username already exists'})
+        else:
+            user.username = username
+            user.set_password(password)
+            user.save()
+
+            return Response({'Success': 'Create user successfully'})
