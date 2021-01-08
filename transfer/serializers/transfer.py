ba6a1dc93af650512a5c4d32c4c9ef6280ba8404
thiennlinh <nguyenthienlinhptit@gmail.com>
2021-01-08 16:04:23 +0700

    Change result api

--- transfer/serializers/transfer.py
+++ transfer/serializers/transfer.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 class TransferSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transfer
-        fields = ['id', 'tea', 'owner', 'secondary_owner', 'name', 'date', 'status']
+        fields = ['id', 'tea', 'owner', 'secondary_owner', 'name', 'date', 'status', 'secondary_owner']
 
     def to_representation(self, instance):
         response = super().to_representation(instance)
@@ -21,5 +21,16 @@ class TransferSerializer(serializers.ModelSerializer):
         response['owner_name'] = instance.owner.fullname
         response['secondary_owner_address'] = instance.secondary_owner.address
         response['secondary_owner_phone'] = instance.secondary_owner.phone_number
+        response['secondary_owner_dob'] = instance.secondary_owner.dob
+        response['secondary_owner_email'] = instance.secondary_owner.email
+        response['secondary_owner_gender'] = instance.secondary_owner.gender
+        response['secondary_owner_province'] = instance.secondary_owner.province
+        response['secondary_owner_district'] = instance.secondary_owner.district
+        response['secondary_owner_sub_district'] = instance.secondary_owner.sub_district
+        response['secondary_owner_street'] = instance.secondary_owner.street
+        response['secondary_owner_id_card'] = instance.secondary_owner.id_card
+        response['secondary_owner_permanent'] = instance.secondary_owner.permanent_residence
+        response['secondary_owner_issued_by'] = instance.secondary_owner.issued_by
+        response['secondary_owner_issued_date'] = instance.secondary_owner.issued_date
 
         return response
