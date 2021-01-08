336255fd7a919b64262d8e1c3b5dbef10045d449
thiennlinh <nguyenthienlinhptit@gmail.com>
2021-01-08 11:08:54 +0700

    Create api list request

--- teas/apis/v1/teas.py
+++ teas/apis/v1/teas.py
@@ -67,6 +67,14 @@ class TeasView(ReadOnlyModelViewSet):
 
         return Response(transfer)
 
+    @action(detail=True, methods=['get'], url_path='list_request', serializer_class=TransferSerializer)
+    def get_list_request(self, *args, **kwargs):
+        tea = self.get_object()
+        transfer = Transfer.objects.select_related('tea').filter(tea=tea, status='wait_owner_agree')
+        transfer = TransferSerializer(transfer, many=True).data
+
+        return Response(transfer)
+
 
 class TeasAdminView(ViewSetMixin, generics.RetrieveUpdateAPIView, generics.ListCreateAPIView):
     serializer_class = TeasSerializer
