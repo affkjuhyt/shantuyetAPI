16d66b21ed42170440586ee54220c9dd638f4862
thiennlinh <nguyenthienlinhptit@gmail.com>
2021-01-11 18:23:51 +0700

    Add status tea

--- teas/models/teas.py
+++ teas/models/teas.py
@@ -9,6 +9,16 @@ logger = logging.getLogger(__name__.split('.')[0])
 
 
 class Teas(BaseTimeStampModel):
+    APPROVED = 'approved'
+    REJECT = 'reject'
+    PROCESSING = 'processing'
+
+    TEA_STATUS = (
+        (APPROVED, 'Approved'),
+        (REJECT, 'Reject'),
+        (PROCESSING, 'Processing')
+    )
+
     owner = models.ForeignKey(Owner, null=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=1000, null=True)
     age = models.IntegerField(null=True, blank=True)
@@ -17,6 +27,7 @@ class Teas(BaseTimeStampModel):
     lat = models.DecimalField(max_digits=15, decimal_places=9)
     lon = models.DecimalField(max_digits=15, decimal_places=9)
     tree_area = models.CharField(max_length=1000, null=True, blank=True)
+    status = models.CharField(max_length=20, choices=TEA_STATUS, default=PROCESSING)
     image1 = models.ImageField(upload_to="teas/%Y/%m/%d", null=True, blank=True)
     image2 = models.ImageField(upload_to="teas/%Y/%m/%d", null=True, blank=True)
     image3 = models.ImageField(upload_to="teas/%Y/%m/%d", null=True, blank=True)
