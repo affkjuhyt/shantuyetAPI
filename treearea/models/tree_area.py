46a8cffd04dd4f37bcf5c78763393be01a293ea4
thiennlinh <nguyenthienlinhptit@gmail.com>
2021-02-01 16:33:37 +0700

    Count number tea of a tree area

--- treearea/models/tree_area.py
+++ treearea/models/tree_area.py
@@ -8,12 +8,15 @@ logger = logging.getLogger(__name__.split('.')[0])
 class TreeArea(models.Model):
     name = models.CharField(max_length=50)
     acreage = models.FloatField(max_length=20, null=True, blank=True)
-    number_tea = models.IntegerField(blank=True, null=True)
     content = models.CharField(max_length=2000, null=True, blank=True)
     image = models.ImageField(upload_to="tree_area/%Y/%m/%d", null=True, blank=True)
 
     def __str__(self):
         return "%s" % self.name
+
+    @property
+    def number_tea(self):
+        return self.teas_set.all().count()
 
 
 class Coordinate(models.Model):
